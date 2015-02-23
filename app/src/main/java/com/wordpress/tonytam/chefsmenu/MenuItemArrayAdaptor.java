04f039dbe57d575924942a26eaf0a943e84b7139
Tony Tam <tonytam@yahoo-inc.com>
2015-02-22 22:56:38 -0800

    first version

---
+++ app/src/main/java/com/wordpress/tonytam/chefsmenu/MenuItemArrayAdaptor.java
@@ -0,0 +1,41 @@
+package com.wordpress.tonytam.chefsmenu;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.wordpress.tonytam.chefsmenu.R;
+import com.wordpress.tonytam.chefsmenu.dummy.MenuContent;
+
+import java.util.List;
+
+/**
+ * Created by yahoo on 2/22/15.
+ */
+public class MenuItemArrayAdaptor extends ArrayAdapter<MenuContent.MenuItem> {
+    private final Context context;
+    private final List<MenuContent.MenuItem> values;
+
+    public MenuItemArrayAdaptor(Context context, List<MenuContent.MenuItem> values) {
+        super(context, R.layout.main_menu_navigation, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.main_menu_navigation, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.menu_id);
+        textView.setText(values.get(position).toString());
+        // Change the icon for Windows and iPhone
+        String s = values.get(position).toString();
+
+        return rowView;
+    }
+}
---
+++ app/src/main/java/com/wordpress/tonytam/chefsmenu/dummy/MenuContent.java
@@ -0,0 +1,56 @@
+package com.wordpress.tonytam.chefsmenu.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p/>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class MenuContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static List<MenuItem> ITEMS = new ArrayList<MenuItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static Map<String, MenuItem> ITEM_MAP = new HashMap<String, MenuItem>();
+
+    static {
+        // TODO: Get this from a JSON data store locally that is refreshed
+        // offline
+        addItem(new MenuItem("1", "Dinner Menu"));
+        addItem(new MenuItem("2", "Drink Menu"));
+        addItem(new MenuItem("3", "Dessert Menu"));
+    }
+
+    private static void addItem(MenuItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class MenuItem {
+        public String id;
+        public String content;
+
+        public MenuItem(String id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
